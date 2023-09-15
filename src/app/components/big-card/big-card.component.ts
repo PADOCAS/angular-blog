52b4c0fdc360cb94af9f7551ea08f7252c619ed0
Luciano Decio Neto <lucianodeciopacheco@gmail.com>
2023-09-15 14:05:47 -0300

    Ajustes Blog (componentes dinâmicos)

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./big-card.component.css']
 })
 export class BigCardComponent {
-
+  //Deixando as propriedade inputáveis para serem alimentadas ao chamar o componente:
+  @Input() photoCover:string = "";
+  @Input() cardTitle:string = "";
+  @Input() cardDescription:string = "";
 }
--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
-  styleUrls: ['./small-card.component.css']
+  styleUrls: ['./small-card.component.css'],
 })
 export class SmallCardComponent {
-
+  // Propriedades inputáveis
+  @Input() photoCover: string = "";
+  @Input() cardTitle: string = "";
 }
