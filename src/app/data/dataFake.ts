d61e024f08dd87e764331705c82a8c82c165bf09
Luciano Decio Neto <lucianodeciopacheco@gmail.com>
2023-09-15 18:23:17 -0300

    Ajustes Blog

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,26 @@
 export const dataFake = [
-{
-  id: "1",
-  photo: '',
-  title: 'teste',
-  description: 'Ola',
-},
-{
-  id: "2",
-  photo: '',
-  title: 'teste',
-  description: 'Ola',
-}
+  {
+    id: '1',
+    photo: 'https://cdn.espn.com.br/image/wide/622_9ed38eb3-b595-3d4e-97ae-9525776648d9.jpg',
+    title: 'Mundial 2012',
+    description: 'Corinthians domina no Japão! Japoneses espantados com torcida do Corinthians.',
+  },
+  {
+    id: '2',
+    photo: 'https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/blte015bc5a8a93a16a/634216dc762be510f72ffedc/getty_arena_corinthians.jpg?auto=webp&format=pjpg&width=3840&quality=60',
+    title: 'Neo Química bate mais um record no Brasil',
+    description: 'Neo Química bate mais um record no Brasil',
+  },
+  {
+    id: '3',
+    photo: 'https://noticiasdotimao.com.br/wp-content/uploads/2023/06/FQWw9YXUYAUwSx-.webp',
+    title: 'Mascote de cara nova',
+    description: 'Mascote de cara nova',
+  },
+  {
+    id: '4',
+    photo: 'https://centraldotimao.com.br/wp-content/uploads/2019/11/bdf68f07-d614-4a4c-bbf7-f751cd641ef2.jpg.640x360_q75_box-00640359_crop_detail.jpg',
+    title: 'Gigante Cássio pega mais um pênalti',
+    description: 'Gigante Cássio pega mais um pênalti',
+  },
 ];
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+//Importando nosso arquivo dataFake para alimentação de dados:
+import { dataFake } from '../../data/dataFake';
 
 @Component({
   selector: 'app-content',
@@ -7,19 +9,28 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./content.component.css'],
 })
 export class ContentComponent implements OnInit {
-  photoCover: string =
-    'https://cdn.meutimao.com.br/fotos-do-corinthians/w941/2022/09/05/cassio_foi_o_goleiro_titular_do_corinthians_yryv.jpg';
-  contentTitle: string = 'asdsad';
-  contentDescription: string = 'asdasd';
+  photoCover: string = "";
+  contentTitle: string = "";
+  contentDescription: string = "";
+  private id: string | null = '0';
 
   //Ativa o Route:
-  constructor(private route:ActivatedRoute) {
-  }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     //Verificar o ID que está recebendo na route content como parametro:
-    this.route.paramMap.subscribe(value =>
-      console.log(value.get("id"))
-    )
+    // Ao iniciar o component já vai alimentar o id do this.contentDescription.component:
+    this.route.paramMap.subscribe((value) => (this.id = value.get('id')));
+    this.setValuesToComponent(this.id);
+  }
+
+  setValuesToComponent(id: string | null) {
+    // Vamos procurar no nosso dataFake o ID que está vindo
+    const result = dataFake.filter((article) => article.id == id)[0];
+
+    // Alimenta os atributos:
+    this.photoCover = result.photo;
+    this.contentTitle = result.title;
+    this.contentDescription = result.description;
   }
 }
